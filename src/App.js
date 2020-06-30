52db9404350a1218ab29b58ed75a3a0443e5e401
Sebastian Dønnum <sebbisen95@gmail.com>
2020-06-30 19:21:42 +0200

    midway in tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
 
-
-import {Cards, Chart, CountryPicker} from './components';
-import styles from './App.module.css';
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData, fetchDailyData } from "./api";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    const yo = await fetchDailyData();
+
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
-    )
+    );
   }
 }
 
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+
+import { countries } from "../../api";
+
+import styles from "./CountryPicker.module.css";
 
 const CountryPicker = () => {
-    return (
-        <h1>CountryPicker</h1>
-    )
-}
+  const [fetchedCountries, setFetchedCountries] = useState([]);
 
-export default CountryPicker
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setFetchedCountries(await countries);
+    };
+
+    fetchCountries();
+  }, [setFetchedCountries]);
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default CountryPicker;
