a9c8a1c66cd551d5cf7ef06a093652537cc7f0c5
Sebastian Dønnum <sebbisen95@gmail.com>
2020-07-01 15:28:55 +0200

    Updates

---
+++ precache-manifest.0ddb4ea44c061967ab1fd84a05a1f52b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025643d5de01e52a4dcfb8c0df6c37b6",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "a410bdc4e270ee10b619",
+    "url": "/covidtracker/static/css/main.f4a745ab.chunk.css"
+  },
+  {
+    "revision": "1f27ea8cd67fbf85d1ba",
+    "url": "/covidtracker/static/js/2.9ffbe320.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.9ffbe320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a410bdc4e270ee10b619",
+    "url": "/covidtracker/static/js/main.34381c74.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/covid.d7265326.png"
+  }
+]);
